683d9ca4938e1434746eb4b70bb92ad931410a6d
unknown <ekorosi2000@gmail.com>
2026-07-09 15:11:24 +0100

    Add EKPT web app icons

--- frontend/app/+html.tsx
+++ frontend/app/+html.tsx
@@ -12,6 +12,10 @@ export default function Root({ children }: PropsWithChildren) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <link rel="icon" href="/favicon.png" />
+<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="EKPT Tracker" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
         {/*
           Disable body scrolling on web to make ScrollView components work correctly.
           If you want to enable scrolling, remove `ScrollViewStyleReset` and
