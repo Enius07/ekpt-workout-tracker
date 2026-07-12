a6dc3af1b68670eb19d99d173b13819df435ba49
unknown <ekorosi2000@gmail.com>
2026-07-12 13:56:46 +0100

    Add PWA manifest

--- frontend/app/+html.tsx
+++ frontend/app/+html.tsx
@@ -6,32 +6,37 @@ export default function Root({ children }: PropsWithChildren) {
   return (
     <html lang="en" style={{ height: "100%" }}>
       <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <link rel="icon" href="/favicon.png" />
-<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-<meta name="apple-mobile-web-app-title" content="EKPT Tracker" />
-<meta name="apple-mobile-web-app-capable" content="yes" />
-        {/*
-          Disable body scrolling on web to make ScrollView components work correctly.
-          If you want to enable scrolling, remove `ScrollViewStyleReset` and
-          set `overflow: auto` on the body style below.
-        */}
-        <ScrollViewStyleReset />
-        <style
-          dangerouslySetInnerHTML={{
-            __html: `
-              body > div:first-child { position: fixed !important; top: 0; left: 0; right: 0; bottom: 0; }
-              [role="tablist"] [role="tab"] * { overflow: visible !important; }
-              [role="heading"], [role="heading"] * { overflow: visible !important; }
-            `,
-          }}
-        />
-      </head>
+  <meta charSet="utf-8" />
+  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+  />
+
+  <link rel="manifest" href="/manifest.json" />
+  <meta name="theme-color" content="#000000" />
+
+  <link rel="icon" href="/favicon.png" />
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+  <meta name="apple-mobile-web-app-title" content="EKPT Tracker" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+
+  {/*
+    Disable body scrolling on web to make ScrollView components work correctly.
+    If you want to enable scrolling, remove `ScrollViewStyleReset` and
+    set `overflow: auto` on the body style below.
+  */}
+  <ScrollViewStyleReset />
+  <style
+    dangerouslySetInnerHTML={{
+      __html: `
+        body > div:first-child { position: fixed !important; top: 0; left: 0; right: 0; bottom: 0; }
+        [role="tablist"] [role="tab"] * { overflow: visible !important; }
+        [role="heading"], [role="heading"] * { overflow: visible !important; }
+      `,
+    }}
+  />
+</head>
       <body
         style={{
           margin: 0,
